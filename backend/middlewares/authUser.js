551857e7f15b081bc59441968db3989f5e94334c
vikash-sharma-4991 <vikashsharma4991@gmail.com>
2026-05-21 18:18:12 +0530

    Profile View and Update features

---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+
+
+//User Authentication Middleware
+
+const authUser = async(req, res,next) => {
+    try{
+        const {token} = req.headers
+        console.log("Token:",token )
+        //token check
+        if(!token){
+            return res.json({success:false, message:"Not Authorized Login Again"})
+        }
+
+        //Verify token
+
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+
+        req.body.userId = token_decode.id
+
+        next()
+
+    }catch(error){
+        console.log(error);
+        return res.json({success:false, message:error.message})
+    }
+}
+
+
+export default authUser;
